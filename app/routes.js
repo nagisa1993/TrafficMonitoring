3d04fbdff93fa1a1ea76d29243eb89fe69ca88ac
nagisa1993 <mahaiyue2015@163.com>
2016-10-19 20:27:39 -0400

    database model added
    1. Added database model schema;
    2. Debugged POST req (success);
    NEED FIX:
    1. *responsive sidebar to device-size

---
+++ app/routes.js
@@ -0,0 +1,53 @@
+// Load the model
+var History = require('./models/model');
+
+// Opens App routes RESTful design
+module.exports = function(app) {
+
+    // api ---------------------------------------------------------------------
+
+    // get all histories
+    app.get('/api/historys', function(req, res) {
+
+        // use mongoose to get all histories in the database
+        History.find(function(err, historys) {
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err);
+
+            res.json(historys); // return all histories in JSON format
+        });
+    });
+
+// create history and send back all histories after creation
+    app.post('/api/historys', function(req, res) {
+
+        // create a history
+        var newhistory = new History(req.body);
+        newhistory.save(function(err){
+            if(err)
+                res.send(err);
+
+            res.json(req.body);
+        });
+
+    });
+
+// delete a history
+    app.delete('/api/historys/:history_id', function(req, res) {
+        History.remove({
+            _id : req.params.todo_id
+        }, function(err, history) {
+            if (err)
+                res.send(err);
+
+            // get and return all the histories after you create another
+            History.find(function(err, historys) {
+                if (err)
+                    res.send(err);
+
+                res.json(historys);
+            });
+        });
+    });
+};
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var express = require("express");
 var app = express();
 var logger = require("morgan");
-//var mongoose = require("mongoose");
+var mongoose = require("mongoose");
 var path = require("path");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-//var database = require(".data/database");
+var database = require("./data/database");
 var port = process.env.PORT || 3000;         // set the port
 
 
-//mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
+mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 app.use(express.static(__dirname + '/public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());   // parse application/json
@@ -19,11 +19,11 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride());
 
 // routes ===================================.===================================
-//require('./app/routes.js')(app);
-app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: __dirname });
-});
+require('./app/routes.js')(app);
+// app.all('/*', function(req, res, next) {
+//     // Just send the index.html for other files to support HTML5Mode
+//     res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+// });
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
