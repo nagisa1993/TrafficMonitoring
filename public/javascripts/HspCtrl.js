60bc13574b8682048c9a0fd531c278de8e5f2bfa
nagisa1993 <mahaiyue2015@163.com>
2016-11-20 00:01:28 -0500

    Complete Hotspot page form
    1. Completed front-end and back-end ‘/api/hotspots ‘communication.
    2. Added markers to hotspot map.
    3. Supplemented new test cases into /api/hotspots.
    *ISSUES:
    1. markers color difference from severity

--- public/javascripts/HspCtrl.js
+++ public/javascripts/HspCtrl.js
@@ -18,7 +18,11 @@ hotCtrl.controller('HspCtrl', function($scope, $http, $log, $timeout, uiGmapGoog
     // Do stuff with your $scope.
     // Note: Some of the directives require at least something to be defined originally!
     // e.g. $scope.markers = []
-    $scope.formData = {};
+    $scope.formData = {
+        "day": "All",
+        "weather": "All",
+        "severity": "All"
+    };
 /*-------------------------------------------------------------------------------------------------------*/
 /*                                          Draw chart                                                   */
 /*-------------------------------------------------------------------------------------------------------*/
@@ -48,105 +52,46 @@ hotCtrl.controller('HspCtrl', function($scope, $http, $log, $timeout, uiGmapGoog
         zoom: 8,
         fit: true,
         pan: 1,
-        // events: {
-        //     tilesloaded: function (maps, eventName, args) {
-        //     },
-        //     dragend: function (maps, eventName, args) {
-        //     },
-        //     zoom_changed: function (maps, eventName, args) {
-        //     }
-        // }
     };
+
+    //$scope.Markers = [];
     let markers = [];
-    $http.get('/api/weathers')
-    .success(function(data){
-        for(let i = 0; i < data.length; i++){
-            let newmarker = {};
-            newmarker.latitude = data[i].location.lat;
-            newmarker.longitude = data[i].location.lng;
-            newmarker.id = i;
-            markers.push(newmarker);
-        }       
-    })
-    .error(function(data){
-        console.log('Error' + data);
+    $http.post('/api/hotspots', $scope.formData)
+            .success(function (data) {
+                // Once complete, clear the form (except location)
+                console.log("ok" + $scope.formData);
+                let markers = [];
+                for(let i = 0; i < data.length; i++){
+                    let newmarker = {};
+                    newmarker.latitude = data[i].location.lat;
+                    newmarker.longitude = data[i].location.lng;
+                    newmarker.id = i;
+                    markers.push(newmarker);
+                }      
+                $scope.Markers = markers;
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
     });
-    $scope.Markers = markers;
-    // $scope.marker = {
-    //     id: 0,
-    //     coords: {
-    //         latitude: 40.523325,
-    //         longitude: -74.458809
-    //     },
-    //     options: {
-    //         draggable: true
-    //     },
-    //     events: {
-    //         dragend: function (marker, eventName, args) {
-    //             var lat = marker.getPosition().lat();
-    //             var lon = marker.getPosition().lng();
-    //             // $log.log(lat);
-    //             // $log.log(lon);
 
-    //             $scope.marker.options = {
-    //                 draggable: true,
-    //                 labelContent: "",
-    //                 labelAnchor: "100 0",
-    //                 labelClass: "marker-labels"
-    //             };
-    //         }
-    //     }
-    // };
-
-    // uiGmapGoogleMapApi is a promise.
-    // The "then" callback function provides the google.maps object.
-    uiGmapGoogleMapApi
-        .then(function(maps) {
-            $scope.directionsService = new maps.DirectionsService();
-            $scope.directionsDisplay = new google.maps.DirectionsRenderer();
-        });
-
-    uiGmapIsReady.promise(1)                     // this gets all (ready) map instances - defaults to 1 for the first map
-        .then(function(instances) {
-            instances.forEach(function(inst) {   // instances is an array object
-                $scope.maps = inst.map;
-                $scope.directionsDisplay.setMap(inst.maps); // if only 1 map it's found at index 0 of array
-            });
-    });
+    
 /*-------------------------------------------------------------------------------------------------------*/
 /*                                          Form Controller                                              */
 /*-------------------------------------------------------------------------------------------------------*/
     $scope.update = function() {
-        // console.log($scope.ori_detail.name);
-        // console.log($scope.des_detail.name);
-        var request = {
-            origin: $scope.ori_detail.geometry.location,
-            destination: $scope.des_detail.geometry.location,
-            travelMode: google.maps.TravelMode.DRIVING,
-            optimizeWaypoints: true
-        }
-        $scope.directionsDisplay.setMap($scope.maps);
-        $scope.directionsService.route(request, function(response, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                $scope.directionsDisplay.setDirections(response);
-                // for (var i = 0, len = response.routes.length; i < len; i++) {
-                //     new google.maps.DirectionsRenderer({
-                //         map: $scope.maps,
-                //         directions: response,
-                //         routeIndex: i
-                //     });
-                // }
-            }
-        });
-
-        $scope.marker = {};
-
-        // Saves the user data to the db
-        $http.post('', $scope.formData)
+        // get all incidents according to form data
+        $http.post('/api/hotspots', $scope.formData)
             .success(function (data) {
                 // Once complete, clear the form (except location)
-                $scope.formData = {};
-
+                let markers = [];
+                for(let i = 0; i < data.length; i++){
+                    let newmarker = {};
+                    newmarker.latitude = data[i].location.lat;
+                    newmarker.longitude = data[i].location.lng;
+                    newmarker.id = i;
+                    markers.push(newmarker);
+                }      
+                $scope.Markers = markers;
             })
             .error(function (data) {
                 console.log('Error: ' + data);
